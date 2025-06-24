fe2e8db11b27913e18aaaf0a1b470b695f65a6d7
Lindist <four181049@gmail.com>
2025-06-24 08:46:55 +0700

    switching achivement con_

--- cicker_ar.js
+++ cicker_ar.js
@@ -2,26 +2,32 @@ const popup_ac = document.querySelectorAll(".achivement");
 const detail_ac = document.querySelector(".detail_achivement");
 const cancels = document.querySelectorAll(".modal-content-achivement ion-icon");
 const closes = document.querySelectorAll(".modal-content-achivement button");
+const modals = document.querySelectorAll(".modal-content-achivement");
+const achivements = document.querySelectorAll(".achivement");
 
-popup_ac.forEach((ac) => {
+popup_ac.forEach((ac,index) => {
     ac.onclick = () =>{
         detail_ac.style.display = "flex";
         detail_ac.style.alignItems="center";
         detail_ac.style.justifyContent = "center";
-        detail_ac.firstElementChild.style.animation = "scaleup 0.5s ease";
+        
+        modals[index].style.display = "flex";
+        modals[index].style.animation = "scaleup 0.5s ease";
     }
 });
 
 cancels.forEach((cancel,i) => {
     cancel.onclick = () => {
-        detail_ac.firstElementChild.style.animation = "scaledown 0.5s ease";
+        modals[i].style.animation = "scaledown 0.5s ease";
         setTimeout(() => {
+            modals[i].style.display = "none";
             detail_ac.style.display = "none";
         },500)
     }
     closes[i].onclick = () => {
-        detail_ac.firstElementChild.style.animation = "scaledown 0.5s ease";
+        modals[i].style.animation = "scaledown 0.5s ease";
         setTimeout(() => {
+            modals[i].style.display = "none";
             detail_ac.style.display = "none";
         },500)
     }
